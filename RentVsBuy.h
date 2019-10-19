787b79caee16fdeda3a887cfb5aea5c30d38e4d9
rayngan999 <rayngan999@gmail.com>
2019-10-19 14:54:58 -0700

    9

---
+++ RentVsBuy.h
@@ -0,0 +1,27 @@
+//
+// Created by T Alpha 1 on 10/13/2019.
+//
+
+#ifndef RENTVSBUY__RENTVSBUY_H_
+#define RENTVSBUY__RENTVSBUY_H_
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <tgmath.h>
+
+void HomeBuyingInfo();
+double CommuteFromHome();
+void RentingInfo();
+double CommuteFromApartment();
+double modeOfTransportation();
+
+
+
+class commute{
+ public:
+  double cost();
+};
+#endif //RENTVSBUY__RENTVSBUY_H_
---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include "RentVsBuy.h"
+#include <fstream>
+#include <iostream>
+
+void PrintFile(std::ostream& out, const std::string& fileName){
+  std::ifstream infile(fileName);
+  char c;
+  while((c = infile.get()) != EOF){
+    out << c;
+  }
+}
+
+int main(){
+  HomeBuyingInfo();
+  RentingInfo();
+  //RunRentVsBuy();
+  PrintFile(std::cout, "RentVsBuy.csv");
+  return 0;
+}
